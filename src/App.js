cc55400452e7bb431adb86f8d57128e9e927e698
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2021-12-06 11:58:08 +0330

    Added title and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React, { useState } from 'react';
+import Container from '@mui/material/Container';
 
 function App() {
-  return (
-    <div className="App bg-dark-blue">
+  const [color, setColor] = useState('blue')
 
+  return (
+    <div className={"App bg-dark-" + color + " light-" + color}>
+      <Container maxWidth="sm">
+        <div id="title">
+          <h1>Tic-Tac-Toe</h1>
+        </div>
+
+        <div id="footer">
+          <p>Designed by <b>Saeed Ahmadnia</b></p>
+          <p>December 2021</p>
+        </div>
+      </Container>
     </div>
   );
 }
